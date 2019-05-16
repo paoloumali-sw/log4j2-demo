f5e3f643b03db987082f06ab035d82291d531bd8
Paolo Umali <paolo.umali@solarwinds.cloud>
2019-05-17 02:31:37 +0800

    welcome controller logs

--- src/main/java/com/example/log4j2demo/WelcomeController.java
+++ src/main/java/com/example/log4j2demo/WelcomeController.java
@@ -1,13 +1,23 @@
 package com.example.log4j2demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class WelcomeController {
-    
+
+    private static final Logger logger = LogManager.getLogger(WelcomeController.class);
+
     @RequestMapping("/")
     public String index() {
+        
+        logger.debug("Debugging log from WelcomeController");
+        logger.info("Info log from WelcomeController");
+        logger.warn("Hey, This is a warning! from WelcomeController");
+        logger.error("Oops! We have an Error. OK from WelcomeController");
+
         return "Greetings from Spring Boot WelcomeController!";
     }
     
