4183aa5cdc9ccfd459a23f9616b2c6e82d029e59
Paolo Umali <paolo.umali@solarwinds.cloud>
2019-05-17 02:31:46 +0800

    hello controller logs

--- src/main/java/com/example/log4j2demo/HelloController.java
+++ src/main/java/com/example/log4j2demo/HelloController.java
@@ -1,13 +1,22 @@
 package com.example.log4j2demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
+    private static final Logger logger = LogManager.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String index() {
+        logger.debug("Debugging log from HelloController");
+        logger.info("Info log from HelloController");
+        logger.warn("Hey, This is a warning! from HelloController");
+        logger.error("Oops! We have an Error. OK from HelloController");
+
         return "Greetings from Spring Boot HelloController!";
     }
     
